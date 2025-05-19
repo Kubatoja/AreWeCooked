a3fc71ac312fc293a5b5a9639bbf708b2d51f99e
Kubatoja <jakub1720@gmail.com>
2025-05-20 00:59:04 +0200

    bugfix

--- AreWeCooked/src/Components/MainMenu.jsx
+++ AreWeCooked/src/Components/MainMenu.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../Styles/mainMenu.css";
+import "../Styles/mainmenu.css";
 
 function MainMenu({ setGameState, setLobbyId, setIsHost }) {
   function generateLobbyId() {
